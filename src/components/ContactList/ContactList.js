cf7f9208c45de5f171937abb5b7a0faacbe5e3b1
Viktor Poklad <3d.poklad@gmail.com>
2021-10-11 22:34:54 +0300

    чистка

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -6,12 +6,6 @@ export default function ContactList({ contacts, handleDelItem }) {
       <h2 className={s.title}>Contacts</h2>
       <ul>
         {contacts.map(contact => (
-          //       <li key={contact.id}>
-          //   {contact.name}: <span>{contact.number}</span>
-          //   <button type="button" id={contact.id} onClick={() => handleDelItem(contact.id)}>
-          //     del
-          //   </button>
-          // </li>
           <ContactItem
             key={contact.id}
             contact={contact}
--- src/components/ContactList/Filter.js
+++ src/components/ContactList/Filter.js
@@ -11,16 +11,5 @@ export default function Filter({ value = ' ', handlChange }) {
         onChange={handlChange}
       />
     </>
-    // <label>
-    //   Find contacts by name
-    //   <input
-    //     type="text"
-    //     name="filter"
-    //     value={value}
-    //     onChange={handlChange}
-    //     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-    //     title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-    //   />
-    // </label>
   );
 }
